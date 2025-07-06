6a5a603617e5e8a2d537519b3ee109a5d07640bd
Michael L Perry <michael@qedcode.com>
2025-07-05 22:23:01 -0500

    Remove ESLint configuration for player-ip and update test script in service-ip package.json

--- app/player-ip/.eslintrc.cjs
+++ app/player-ip/.eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
