6630592547f150a497afc1b6f5bef72e5584d0a9
Michael L Perry <michael@qedcode.com>
2025-07-12 21:22:04 -0500

    Use an in-memory Jinaga client in dev mode

--- app/gamehub-admin/src/jinaga-config.ts
+++ app/gamehub-admin/src/jinaga-config.ts
@@ -5,9 +5,9 @@ import { BearerAuthenticationProvider } from "./auth/BearerAuthenticationProvide
 export const authProvider = new BearerAuthenticationProvider();
 
 export const j = JinagaBrowser.create({
-    indexedDb: "jinaga-launchkings-admin",
-    httpEndpoint: import.meta.env.VITE_REPLICATOR_URL,
-    httpAuthenticationProvider: authProvider
+    indexedDb: import.meta.env.DEV ? undefined : "jinaga-gamehub-admin",
+    httpEndpoint: import.meta.env.DEV ? undefined : import.meta.env.VITE_REPLICATOR_URL,
+    httpAuthenticationProvider: import.meta.env.DEV ? undefined : authProvider
 });
 
 // Export authorization rules for the replicator
