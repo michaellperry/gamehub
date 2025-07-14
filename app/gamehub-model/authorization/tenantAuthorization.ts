c889367a701a7585a6b2c1230944ac6acb8681db
Michael L Perry <michael@qedcode.com>
2025-07-13 20:03:04 -0500

    Allow the creator of a tenant to create administrators

--- app/gamehub-model/authorization/tenantAuthorization.ts
+++ app/gamehub-model/authorization/tenantAuthorization.ts
@@ -2,9 +2,12 @@ import { AuthorizationRules } from "jinaga";
 import { Administrator, Tenant } from "../model/index.js";
 
 export const tenantAuthorization = (a: AuthorizationRules) => a
-  // Only the creator can create a tenant
-  .type(Tenant, tenant => tenant.creator.predecessor())
-  
-  // Administrators can add other administrators to their tenant
-  .type(Administrator, admin => Administrator.usersOf(admin.tenant))
-  ;
+    // Only the creator can create a tenant
+    .type(Tenant, tenant => tenant.creator.predecessor())
+
+    // The creator of a tenant can create administrators for that tenant
+    .type(Administrator, admin => admin.tenant.creator.predecessor())
+
+    // Administrators can add other administrators to their tenant
+    .type(Administrator, admin => Administrator.usersOf(admin.tenant))
+    ;
