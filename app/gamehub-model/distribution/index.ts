01c4e47389bfe0e39f02a564109ead5972587916
Michael L Perry <michael@qedcode.com>
2025-08-17 14:02:58 -0500

    A user can see their own player name.

--- app/gamehub-model/distribution/index.ts
+++ app/gamehub-model/distribution/index.ts
@@ -1,7 +1,12 @@
 import { DistributionRules } from 'jinaga';
 import { bookkeepingDistribution } from './bookkeepingDistribution.js';
 import { challengeDistribution } from './challengeDistribution.js';
+import { playerDistribution } from './playerDistribution.js';
 import { tenantDistribution } from './tenantDistribution.js';
 
-export const distribution = (d: DistributionRules) =>
-    d.with(tenantDistribution).with(bookkeepingDistribution).with(challengeDistribution);
+export const distribution = (d: DistributionRules) => d
+    .with(tenantDistribution)
+    .with(playerDistribution)
+    .with(bookkeepingDistribution)
+    .with(challengeDistribution)
+    ;
---
+++ app/gamehub-model/distribution/playerDistribution.ts
@@ -0,0 +1,16 @@
+import { DistributionRules, User } from 'jinaga';
+import { Player, PlayerName, Tenant, model } from '../model/index.js';
+
+export const playerDistribution = (r: DistributionRules) =>
+    r
+        // Allow users to see their own player data and names
+        .share(
+            model.given(User, Tenant).match((user, tenant) =>
+                Player.in(tenant)
+                    .join(player => player.user, user)
+                    .selectMany(player => PlayerName.current(player))
+            )
+        )
+        .with(model.given(User, Tenant).match((user, tenant) =>
+            user
+        ));
